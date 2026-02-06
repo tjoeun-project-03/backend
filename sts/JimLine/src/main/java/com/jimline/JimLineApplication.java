19908648888c926bb190ec13ffb67ee045f0e83b
In-Fo <204827956+JSLEE56@users.noreply.github.com>
2026-02-06 14:09:22 +0900

    sts 생성

---
+++ sts/JimLine/src/main/java/com/jimline/JimLineApplication.java
@@ -0,0 +1,13 @@
+package com.jimline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JimLineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JimLineApplication.class, args);
+	}
+
+}
---
+++ sts/JimLine/src/test/java/com/jimline/JimLineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jimline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JimLineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
